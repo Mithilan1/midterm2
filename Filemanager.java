580086a3999ad2cd14e637f44677aadd6de81f1f
Mithilan1 <mithilan.thanabalasingam@dcmail.ca>
2026-03-11 14:28:32 -0400

    initial commit by mithilan

---
+++ Filemanager.java
@@ -0,0 +1,13 @@
+public class FileManager {
+    public void createFile(String fileName) {
+        System.out.println("Creating file: " + fileName);
+    }
+
+    public void readFile(String fileName) {
+        System.out.println("Reading file: " + fileName);
+    }
+
+    public void deleteFile(String fileName) {
+        System.out.println("Deleting file: " + fileName);
+    }
+}
